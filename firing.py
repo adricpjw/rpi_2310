6d3d1a9dc5e768b386ac79aeae2f35f9addbb036
Adric <adricpjw@gmail.com>
2020-04-10 09:34:15 +0800

    updated naming

--- firing.py
+++ firing.py
@@ -1,29 +1,66 @@
-import RPi.GPIO as GPIO
-import time 
+#!/usr/bin/env python
+import RPi.GPIO as GPIO    
+import time
+from Stepper import StepperMove
+from servo import servoMove
+from spinDC import DCMove
+import rospy
+from std_msgs.msg import String
 
 GPIO.setmode(GPIO.BOARD)
 
-class servoMove:
+#stepper motor
+A1 =13 
+A2 =11
+B1 =15
+B2 =12
 
-    def __init__(self, pin):
-        self.pin = pin
-        GPIO.setup(pin,GPIO.OUT)
+servo_pin = 22
 
-    def act(self):
-        pwm = GPIO.PWM(pin,50)
-        pwm.start(0)
-        pwm.ChangeDutyCycle(7)
-        time.sleep(5)
-        pwm.ChangeDutyCycle(2)
-        
-    def cleanup(self):
+EN1 = 37
+In1 = 33
+In2 = 36
+
+mover = StepperMove(A1,A2,B1,B2)
+servo = servoMove(servo_pin)
+spinner = DCMove(EN1,In1,In2)
+
+val = ''
+
+def getSignal(msg):
+    global val = msg
+
+
+def move(signal):
+    if signal == '-1':
+        mover.down(0.03,2)
+    elif signal == '1':
+        mover.up(0.03,2)
+
+
+def main():
+    global val
+    rospy.init_node('aimingNode',anonymous = True)
+
+    rospy.Subscriber('cmd_stepper',String,getSignal)
+    rate = rospy.Rate(10)
+
+    toggle = True
+    
+    while toggle:
+        if val != '10' and val:
+            move(val)
+        elif val == '10':
+            spinner.turnOn()
+            time.sleep(2)
+            servo.act()
+            time.sleep(3)
+            toggle = False
+            GPIO.cleanup()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
         GPIO.cleanup()
-
-#sero
-servo_point = 22
-GPIO.setup(servo_point,GPIO.OUT)
-pwm = GPIO.PWM(servo_point,50)
-pwm.start(0)
-pwm.ChangeDutyCycle(7)
-time.sleep(5)
-pwm.ChangeDutyCycle(2)
+            
